24caaa861311a697c39f08e51f835d246f24be22
Oscar Martins <oscarrafaelcampos@gmail.com>
2019-02-25 14:12:45 +0000

    server gambiarra tmp

--- js/orcservices.js
+++ js/orcservices.js
@@ -2,8 +2,9 @@
  * dependency injection
  *  - jquery
  *  - axios
+ * 'https://orcseven.com/api/orcv2'
  */
-const API_URL_PATH = window.location.hostname === 'localhost' ? 'http://localhost:8081/orcv2' : 'https://orcseven.com/api/orcv2'
+const API_URL_PATH = window.location.hostname === 'localhost' ? 'http://localhost:8081/orcv2' : 'http://151.236.61.70:8081/api/orcv2'
 
 function httpRequestApi (method, params) {
     return axios({
